ea8f9ac679e45c973579e4114490cf8d64094327
yingy4 <yingy4@hotmail.com>
2016-05-01 19:37:01 +0800

    Update Nuke2.java

--- hw1/Nuke2.java
+++ hw1/Nuke2.java
@@ -2,10 +2,26 @@
 
 import java.io.*;
 
+  /** A class whose main method reads a string from the keyboard and prints the same string, with its
+   *  second character removed.
+   */
+
 class Nuke2 {
+  
+  /** reads a string from the keyboard and prints the same string, with its
+   *  second character removed.
+   *  @param arg is not used.
+   *  @exception Exception thrown if there are any problems parsing the 
+   *             user's input.
+   */
+  
+  public static void main(String[] arg) throws Exception {
+    
+    BufferedReader keyboard;
+    String inputLine;
+    keyboard = new BufferedReader(new InputStreamReader(System.in));
+    inputLine = keyboard.readLine();
+    System.out.println(inputLine.substring(0,1)+inputLine.substring(2));
 
-
-
-
-
+  }
 }
